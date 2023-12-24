495584194c1d3eb3dbe7274a25df017e569c38ec
김선일 <gimseon-il@gimseon-il-ui-MacBookPro.local>
2023-12-24 15:35:39 +0900

    add : Dockerfile 추가 + redis띄워서 확인

--- app/index.ts
+++ app/index.ts
@@ -3,6 +3,8 @@ import * as redis from "redis";
 import { createApp } from "./app";
 dotenv.config();
 const { PORT, REDIS_URL } = process.env;
+console.log("PORT", PORT);
+console.log("REDIS", REDIS_URL);
 if (!PORT || REDIS_URL) throw new Error("PORT or REDIS_URL is required");
 const startServer = async () => {
   const client = redis.createClient({ url: REDIS_URL });
