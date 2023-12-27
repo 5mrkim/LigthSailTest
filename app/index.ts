f7cab0704d832e4fa414f5fb92e9363387c1fbb4
김선일 <gimseon-il@gimseon-il-ui-MacBookPro.local>
2023-12-27 11:36:44 +0900

    mod:graceful shutdown 공부 과정 커밋

--- app/index.ts
+++ app/index.ts
@@ -12,9 +12,22 @@ const startServer = async () => {
 
   const app = createApp(client);
 
-  app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
+  });
+  return server;
+};
+
+const server = startServer();
+
+const gracefulShutdown = async () => {
+  const _server = await server;
+  _server.close(() => {
+    console.log("gracefulShutdown!");
+    process.exit();
   });
 };
 
-startServer();
+process.on("SIGTERM", gracefulShutdown);
+
+process.on("SIGINT", gracefulShutdown);
